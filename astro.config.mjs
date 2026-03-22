d60961ea2d237421d2bbc82e25b0912131dd997d
mimanchi-dongze <mimanchi-dongze@users.noreply.github.com>
2026-03-22 17:49:43 +0800

    feat: Add KaTeX for math rendering and create a `BaseHead` component to centralize page metadata.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mimanchi-dongze.github.io',
 	base: '/my-astro-blog',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 	i18n: {
 		defaultLocale: 'zh-cn',
 		locales: ['zh-cn', 'en'],
